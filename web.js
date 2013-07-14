4ba629f917ae5490d487034f44be4cc949abb2d4
samir raouf <samir.raouf@gmail.com>
2013-07-14 16:45:08 +0300

    read from file

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
+var fs = require('fs');
+var outfile = 'index.html';
+var buff1 = ("                                ","utf-8")
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
+buff =fs.readFileSync(outfile)
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buff.toString(buff1, "utf-8"));
 });
 
 var port = process.env.PORT || 5000;
