2f1f3370532e94987c4765c42dd28b2215da13aa
samir raouf <samir.raouf@gmail.com>
2013-07-14 17:08:38 +0300

    read from file2

--- web.js
+++ web.js
@@ -1,15 +1,15 @@
 var fs = require('fs');
 var outfile = 'index.html';
-var buff1 = ("                                ","utf-8")
+var buff1 = new Buffer("I am a 32 Byte String          ","utf-8");
 
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
-buff =fs.readFileSync(outfile)
+buff1 =fs.readFileSync(outfile);
 
 app.get('/', function(request, response) {
-  response.send(buff.toString(buff1, "utf-8"));
+  response.send(buff1.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
